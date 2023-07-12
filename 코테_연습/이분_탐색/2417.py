cbc50fe069c3cacd7319d533a9af54a777fd54be
gouz7514 <gouz7514@gmail.com>
2023-07-12 15:11:15 +0900

    Update : 2417 - 다른 사람 풀이

--- 코테_연습/이분_탐색/2417.py
+++ 코테_연습/이분_탐색/2417.py
@@ -1,6 +1,7 @@
 # 정수 제곱근
 # 실버 4
 # https://www.acmicpc.net/problem/2417
+# 나의 풀이
 import sys
 sys.setrecursionlimit(10**6)
 
@@ -30,3 +31,18 @@ def binary_search(target, start, end):
       return binary_search(target, mid + 2, end)
     
 print(binary_search(n, 1, n))
+
+# 다른 사람의 풀이
+# n = int(input())
+
+# l = 0
+# r = 2 ** 32
+
+# while l <= r:
+#   mid = (l + r) // 2
+#   if mid ** 2 < n:
+#     l = mid + 1
+#   else:
+#     r = mid - 1
+
+# print(l)
